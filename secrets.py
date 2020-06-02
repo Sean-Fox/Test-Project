6f4799a3270e2eb9798faad041443929e2b350c0
Sean Fox <47177785+Sean-Fox@users.noreply.github.com>
2020-06-02 16:28:25 +0530

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# Go to http://dev.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key="zicFGSirdBbPj6uJc6hkgDJN7"
+consumer_secret="3NHOWAGYvDeeXzcMNTrcvP9DU20yZkNwinWul341HLC6uavrWC"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token="16852719-e363i7eWgu2QvvSmkVrmRQTI0sbd3oBJJrO3BmiZF"
+access_token_secret="4NUliogpdKQUaRyNIvFFBX89Qol2X8THE6Rs5SVAVi9aD"
